f91be2cae240e93d760b13c9b037b0ee7ee79881
ceccobell <francescobell2001@gmail.com>
2024-06-25 14:57:25 +0200

    [UPD] creato if per gestire gli sconti

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ let km = prompt("Inserisci i chilometri che devi percorrere");
 let eta = prompt("Inserisci la tua eta");
 
 let prezzo_biglietto = km * 0.21;
-console.log(prezzo_biglietto);
+
+if (eta < 18) {
+  prezzo_biglietto -= prezzo_biglietto * 0.2;
+  console.log(prezzo_biglietto);
+} else if (eta > 65) {
+  prezzo_biglietto -= prezzo_biglietto * 0.4;
+  console.log(prezzo_biglietto);
+}
