7bdc9c32ef6f4c2205fb9ed22367c6c5be0e6a0c
ceccobell <francescobell2001@gmail.com>
2024-06-25 15:02:59 +0200

    [UPD]  aggiunta funzione toFixed(2) per stampare i numeri con due cifre decimali

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ let prezzo_biglietto = km * 0.21;
 
 if (eta < 18) {
   prezzo_biglietto -= prezzo_biglietto * 0.2;
-  console.log(prezzo_biglietto);
 } else if (eta > 65) {
   prezzo_biglietto -= prezzo_biglietto * 0.4;
-  console.log(prezzo_biglietto);
 }
+
+prezzo_biglietto = prezzo_biglietto.toFixed(2);
+console.log(prezzo_biglietto);
